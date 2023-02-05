f6a0c4358484984eef31d054ff2b3ed09d396f85
Aidhan <me@aidhan.net>
2023-02-05 12:29:10 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-lg">Hello, world!</h1>
-    </main>
+    <div className="bg-gradient-to-r from-blue-300 to-cyan-300 min-w-screen min-h-screen">
+      <main className="flex justify-around items-center flex-wrap">
+        <header className="p-12 flex-auto">
+          <h1 className="text-6xl font-bold mb-2">Hello, world!</h1>
+          <p className="text-2xl">We pull businesses into this century.</p>
+        </header>
+
+        <nav className="flex flex-auto align-center justify-around text-xl">
+          <a className="px-12" href="">
+            one
+          </a>
+          <a className="px-12" href="">
+            two
+          </a>
+          <a className="px-12" href="">
+            three
+          </a>
+        </nav>
+      </main>
+    </div>
   );
 }
