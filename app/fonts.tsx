92cab011b4384719caab0f9410650557de99ad68
Aidhan <me@aidhan.net>
2023-02-23 11:27:23 +0000

    Update 7 files

--- app/fonts.tsx
+++ app/fonts.tsx
@@ -1,4 +1,11 @@
-import { Inter, Lora, Fira_Code } from "@next/font/google";
+import { Source_Sans_Pro, Inter, Lora, Fira_Code } from "@next/font/google";
+
+export const sourceSansPro = Source_Sans_Pro({
+  variable: "--font-sourcesanspro",
+  weight: ["400", "600", "700", "900"],
+  display: "swap",
+  subsets: ["latin"],
+});
 
 export const inter = Inter({
   variable: "--font-inter",
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,15 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      sans: ["var(--font-inter)"],
+      sans: ["var(--font-sourcesanspro)"],
       serif: ["var(--font-lora)"],
       mono: ["var(--font-firacode)"],
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+      },
+    },
   },
 
   corePlugins: {
@@ -24,13 +28,37 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
     require("daisyui"),
   ],
 
   daisyui: {
     styled: true,
-    themes: ["light", "dark"],
+    themes: [
+      {
+        light: {
+          primary: "#31c4b5",
+          secondary: "#06b6d4",
+          accent: "#ff7099",
+          neutral: "#222222",
+          "base-100": "#f7f7f7",
+          info: "#38bdf8",
+          success: "#4ade80",
+          warning: "#FBD428",
+          error: "#fb7185",
+        },
+        dark: {
+          primary: "#31c4b5",
+          secondary: "#06b6d4",
+          accent: "#ff7099",
+          neutral: "#f7f7f7",
+          "base-100": "#222222",
+          info: "#38bdf8",
+          success: "#4ade80",
+          warning: "#FBD428",
+          error: "#fb7185",
+        },
+      },
+    ],
     base: true,
     utils: true,
     logs: false,
